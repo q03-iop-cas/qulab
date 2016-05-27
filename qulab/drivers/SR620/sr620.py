7835058864451173bfd4a3575a2340b56fd36d3a
Q03 <q03.iop.cas@gmail.com>
2016-05-27 11:10:24 +0800

    fix binary data covert error.

--- qulab/drivers/SR620/sr620.py
+++ qulab/drivers/SR620/sr620.py
@@ -1,34 +1,7 @@
 # -*- coding: utf-8 -*-
 import struct
+import numpy as np
 from visa import VisaIOWarning
-
-def convert(data, mode, expd):
-    """将读取出的二进制数据块转换成列表
-
-    根据说明书第53页中的 C 语音代码改写而成。
-
-    data : 读出的原始二进制数据块以16位整形数排列而成的列表
-    mode : 通过向 SR620 发送 'MODE?' 命令获取
-    expd : 通过向 SR620 发送 'EXPD?' 命令获取
-    """
-    factors=[1.05963812934E-14, 1.05963812934E-14,
-             1.05963812934E-14, 1.24900090270331E-9,
-             1.05963812934E-14, 8.3819032E-8, 0.00390625]
-
-    l = len(data)/4
-    ret = []
-    for i in range(l):
-        sign = (data[i*4+3] < 0)
-        v = 0.0
-        for j in range(4):
-            v = v*65536.0 + (sign and ~data[i*4+3-j] or data[i*4+3-j])
-        v = v*factors[mode]
-        if expd != 0:
-            v = v*1.0e-3
-        if sign:
-            v = -v-1.0
-        ret.append(v)
-    return ret
 
 class Driver(BaseDriver):
     error_command = ''
@@ -105,7 +78,14 @@ class Driver(BaseDriver):
         mode = int(self.query('MODE?'))
         expd = int(self.query('EXPD?'))
         self.write('AUTM 1')
-        return convert(list(struct.unpack('%dH' % 4*count, block)), mode, expd)
+
+        factors=[1.05963812934E-14, 1.05963812934E-14,
+                 1.05963812934E-14, 1.24900090270331E-9,
+                 1.05963812934E-14, 8.3819032E-8, 0.00390625]
+        ret = np.array(list(struct.unpack('<%dq' % count, block))) * factors[mode]
+        if expd != 0:
+            ret = ret*1e-3
+        return ret
 
     def __read(self, size):
         try:
